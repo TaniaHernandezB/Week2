4e12785a61788e0bf62c47ab5556c7a3d2b35410
TaniaHernandezB <86626009+TaniaHernandezB@users.noreply.github.com>
2021-07-06 23:20:56 -0400

    Create easy.py

---
+++ easy.py
@@ -0,0 +1,6 @@
+ListName = ['Bob', 'John', 'Wendy']
+ListLast = ['Duggar', 'Doe', 'Burger']
+ListAge = [35, 19, 27]
+userInput = input("Log in with First and Last Name: ")
+userInput2 = input("Age: ")
+print("Hello, my name is " + userInput + " and my age is " + userInput2 + ".")
